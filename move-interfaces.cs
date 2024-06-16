6b26e72b14ea451893ee191b0a4be75982987692
codeoverdoze <102583736+EmmanuelDomfe@users.noreply.github.com>
2024-06-16 23:02:45 +0000

    interface move console app solution

---
+++ move-interfaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Interfaces
+{
+    // Interface IMovable
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Class Car implements IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Class Bicycle implements IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            car.Move();     // Output: Car is moving
+            bicycle.Move(); // Output: Bicycle is moving
+        }
+    }
+}
+
