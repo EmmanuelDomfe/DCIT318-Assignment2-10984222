845a1d65e65cca23a3bc12255ab79220fbcce89f
codeoverdoze <102583736+EmmanuelDomfe@users.noreply.github.com>
2024-06-16 22:59:13 +0000

    inheritances and method console app

---
+++ Inheritance and Method.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace InheritanceAndMethodOverriding
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); // Output: Some generic sound
+            dog.MakeSound();           // Output: Bark
+            cat.MakeSound();           // Output: Meow
+        }
+    }
+}
+
+
+
